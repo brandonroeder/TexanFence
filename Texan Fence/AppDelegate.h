d90482812e4c08b767c92e591434b8510e883722
Brandon Roeder <brandonsroeder@gmail.com>
2014-04-30 19:59:42 -0500

    Initial Commit

---
+++ Texan Fence/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Texan Fence
+//
+//  Created by Brandon Roeder on 4/30/14.
+//  Copyright (c) 2014 Brandon Roeder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
