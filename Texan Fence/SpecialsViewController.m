a70c2330520b2defd1f07945e78e1941a5ac1042
brandonroeder <brandonsroeder@gmail.com>
2014-05-07 04:49:41 -0500

    Added Specials

--- Texan Fence/SpecialsViewController.m
+++ Texan Fence/SpecialsViewController.m
@@ -33,7 +33,7 @@
     [self.revealButton setAction: @selector( revealToggle: )];
     [self.navigationController.navigationBar addGestureRecognizer: self.revealViewController.panGestureRecognizer];
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background.jpg"]];
-
+    
 }
 
 - (void)didReceiveMemoryWarning
